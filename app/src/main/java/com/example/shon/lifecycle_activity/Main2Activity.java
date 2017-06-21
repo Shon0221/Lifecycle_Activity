36510f84897c8498fb3a0364c63eb875c0414bd8
Shon0221 <yoy285437@gmail.com>
2017-06-21 22:38:12 +0800

    20170621 Add
    加入import arch物件

--- app/src/main/java/com/example/shon/lifecycle_activity/Main2Activity.java
+++ app/src/main/java/com/example/shon/lifecycle_activity/Main2Activity.java
@@ -1,9 +1,10 @@
 package com.example.shon.lifecycle_activity;
 
+import android.arch.lifecycle.LifecycleActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Main2Activity extends AppCompatActivity {
+public class Main2Activity extends LifecycleActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
